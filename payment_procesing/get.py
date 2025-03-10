60d2edeebfd3ea343329dc5524eccd3c915640d4
odinakholiqov <odinakholiqov@outlook.com>
2025-03-10 12:46:27 +0500

    intro to modules and packages

---
+++ payment_procesing/get.py
@@ -0,0 +1,15 @@
+
+
+def get(n):
+    while n < 10:
+        print(n)
+        n += 1
+
+    print("Getting money")
+    print(__name__)
+
+
+if __name__ == "__main__":
+    get(1)
+    print("Testing my get module. Will run some tasks")
+    print("test")
